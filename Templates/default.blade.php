3f8539f2c160b30a19e9dd12eed5b5e0e3dcd188
Erik Campobadal <ConsoleTVs@gmail.com>
2019-11-26 13:44:44 +0100

    Update default.blade.php

--- Templates/default.blade.php
+++ Templates/default.blade.php
@@ -5,7 +5,71 @@
         <title>{{ $invoice->name }}</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <style>
-            h1,h2,h3,h4,p,span,div { font-family: DejaVu Sans; }
+            * {
+                -webkit-box-sizing: border-box;
+                -moz-box-sizing: border-box;
+                box-sizing: border-box;
+            }
+
+            h1,h2,h3,h4,h5,h6,p,span,div { 
+                font-family: DejaVu Sans; 
+                font-size:10px;
+                font-weight: normal;
+            }
+
+            th,td { 
+                font-family: DejaVu Sans; 
+                font-size:10px;
+            }
+
+            .panel {
+                margin-bottom: 20px;
+                background-color: #fff;
+                border: 1px solid transparent;
+                border-radius: 4px;
+                -webkit-box-shadow: 0 1px 1px rgba(0,0,0,.05);
+                box-shadow: 0 1px 1px rgba(0,0,0,.05);
+            }
+
+            .panel-default {
+                border-color: #ddd;
+            }
+
+            .panel-body {
+                padding: 15px;
+            }
+
+            table {
+                width: 100%;
+                max-width: 100%;
+                margin-bottom: 0px;
+                border-spacing: 0;
+                border-collapse: collapse;
+                background-color: transparent;
+
+            }
+
+            thead  {
+                text-align: left;
+                display: table-header-group;
+                vertical-align: middle;
+            }
+
+            th, td  {
+                border: 1px solid #ddd;
+                padding: 6px;
+            }
+
+            .well {
+                min-height: 20px;
+                padding: 19px;
+                margin-bottom: 20px;
+                background-color: #f5f5f5;
+                border: 1px solid #e3e3e3;
+                border-radius: 4px;
+                -webkit-box-shadow: inset 0 1px 1px rgba(0,0,0,.05);
+                box-shadow: inset 0 1px 1px rgba(0,0,0,.05);
+            }
         </style>
     </head>
     <body>
